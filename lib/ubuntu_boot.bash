13bb7840399b2d3f5c9fbd68408b1ba5017d46a7
erikh <erikh@hig.no>
2018-09-26 09:01:10 +0200

    let puppet config set server instead of heredoc

--- lib/ubuntu_boot.bash
+++ lib/ubuntu_boot.bash
@@ -6,10 +6,7 @@ apt-get update
 apt-get -y install puppet-agent
 echo "$(ip a | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | tr -d 'inet ' | grep -v '^127') $(hostname).borg.trek $(hostname)" >> /etc/hosts
 echo "manager_ip_address manager.borg.trek manager" >> /etc/hosts
-cat <<EOF >> /etc/puppetlabs/puppet/puppet.conf
-[main]
-    server = manager.borg.trek
-EOF
+/opt/puppetlabs/bin/puppet config set server manager.borg.trek --section main
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 cat <<EOF >> /etc/netplan/50-cloud-init.yaml
             nameservers:
