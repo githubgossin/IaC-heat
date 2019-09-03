abca368253e3d0b5538589665b7ab8922157d02a
Erik Hjelmås <erik.hjelmas@ntnu.no>
2019-09-03 13:07:42 +0200

    Fix: domain name on windows away from openstacklocal

--- lib/windows_boot.ps1
+++ lib/windows_boot.ps1
@@ -6,6 +6,8 @@ $wc = New-Object net.webclient
 $wc.Downloadfile($puppet_agent_msi_url, $puppet_agent_msi_path)
 cmd /c start /wait msiexec /qn /i $puppet_agent_msi_path /l*v puppet_agent_msi_log.txt PUPPET_MASTER_SERVER=manager.borg.trek
 del $puppet_agent_msi_path
+Set-DnsClientGlobalSetting -SuffixSearchList @("borg.trek")
+Get-NetAdapter | Set-DnsClient -ConnectionSpecificSuffix "borg.trek"
 Add-Content "$env:windir\System32\drivers\etc\hosts" "$(& "C:\Program Files\Puppet Labs\Puppet\bin\facter.bat" networking.ip) $(hostname).borg.trek $(hostname)"
 Add-Content "$env:windir\System32\drivers\etc\hosts" "manager_ip_address manager.borg.trek"
 & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" config set server manager.borg.trek --section main
