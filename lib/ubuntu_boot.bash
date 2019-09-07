f378adfa07897fd7f2f9a5c3e59f889ef59c2793
Erik Hjelmås <erik.hjelmas@ntnu.no>
2019-09-07 22:53:15 +0200

    Update: borg.trek to node.consul

--- lib/ubuntu_boot.bash
+++ lib/ubuntu_boot.bash
@@ -4,8 +4,8 @@ wget -O "$tempdeb" https://apt.puppetlabs.com/puppet6-release-bionic.deb
 dpkg -i "$tempdeb"
 apt-get update
 apt-get -y install puppet-agent
-echo "$(/opt/puppetlabs/bin/facter networking.ip) $(hostname).borg.trek $(hostname)" >> /etc/hosts
-echo "manager_ip_address manager.borg.trek manager" >> /etc/hosts
-/opt/puppetlabs/bin/puppet config set server manager.borg.trek --section main
+echo "$(/opt/puppetlabs/bin/facter networking.ip) $(hostname).node.consul $(hostname)" >> /etc/hosts
+echo "manager_ip_address manager.node.consul manager" >> /etc/hosts
+/opt/puppetlabs/bin/puppet config set server manager.node.consul --section main
 /opt/puppetlabs/bin/puppet config set runinterval 300 --section main
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
--- lib/windows_boot.ps1
+++ lib/windows_boot.ps1
@@ -4,12 +4,12 @@ $puppet_agent_msi_path = Join-Path $ENV:TEMP puppet_agent.msi
 $ErrorActionPreference = "Stop"
 $wc = New-Object net.webclient
 $wc.Downloadfile($puppet_agent_msi_url, $puppet_agent_msi_path)
-cmd /c start /wait msiexec /qn /i $puppet_agent_msi_path /l*v puppet_agent_msi_log.txt PUPPET_MASTER_SERVER=manager.borg.trek
+cmd /c start /wait msiexec /qn /i $puppet_agent_msi_path /l*v puppet_agent_msi_log.txt PUPPET_MASTER_SERVER=manager.node.consul
 del $puppet_agent_msi_path
-Set-DnsClientGlobalSetting -SuffixSearchList @("borg.trek")
-Get-NetAdapter | Set-DnsClient -ConnectionSpecificSuffix "borg.trek"
-Add-Content "$env:windir\System32\drivers\etc\hosts" "$(& "C:\Program Files\Puppet Labs\Puppet\bin\facter.bat" networking.ip) $(hostname).borg.trek $(hostname)"
-Add-Content "$env:windir\System32\drivers\etc\hosts" "manager_ip_address manager.borg.trek"
-& "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" config set server manager.borg.trek --section main
+Set-DnsClientGlobalSetting -SuffixSearchList @("node.consul")
+Get-NetAdapter | Set-DnsClient -ConnectionSpecificSuffix "node.consul"
+Add-Content "$env:windir\System32\drivers\etc\hosts" "$(& "C:\Program Files\Puppet Labs\Puppet\bin\facter.bat" networking.ip) $(hostname).node.consul $(hostname)"
+Add-Content "$env:windir\System32\drivers\etc\hosts" "manager_ip_address manager.node.consul"
+& "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" config set server manager.node.consul --section main
 & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" config set runinterval 300 --section main
 & "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" resource service puppet ensure=running enable=true
